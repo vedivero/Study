0d417f2d7cf6229deb0b3e57991728c02d920df4
vedivero <zzangu0703@naver.com>
2020-12-14 23:44:26 +0900

    javaScript basic grammar

---
+++ Nodejs/syntax/template.js
@@ -0,0 +1,32 @@
+var name = 'egoing';
+
+var letter = 'Dear '+name+'\n Lorem ipsum dolor sit amet,\
+consectetur adipisicing elit,'+name+' sed do eiusmod tempor\
+incididunt ut labore et dolore magna aliqua.\
+Ut enim ad minim veniam, quis nostrud exercitation\
+ullamco laboris nisi ut aliquip ex ea commodo\
+consequat. Duis aute irure dolor in reprehenderit\
+in voluptate velit esse cillum dolore eu fugiat\
+nulla pariatur. Excepteur sint occaecat cupidatat\
+non proident,'+name+'sunt in culpa qui officia\
+deserunt mollit anim id est laborum.'+name;
+
+var letter = `Dear ${name}
+
+Lorem ipsum dolor sit amet,
+consectetur adipisicing elit,${name}
+sed do eiusmod tempor
+incididunt ut labore et dolore magna aliqua.
+Ut enim ad minim veniam, quis nostrud exercitation\
+ullamco laboris nisi ut aliquip ex ea commodo
+consequat. Duis aute irure dolor in reprehenderit
+in voluptate velit esse cillum dolore eu fugiat
+nulla pariatur. Excepteur sint occaecat cupidatat
+non proident,${name} sunt in culpa qui officia
+deserunt mollit anim id est laborum.${name}`;
+
+console.log(letter);
+
+//literal
+var a = 1;
+var a = '1';
