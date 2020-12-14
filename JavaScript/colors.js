77a0a72349b5c9199c28413140a758ff30a13b0e
bliss73 <67643214+bliss73@users.noreply.github.com>
2020-12-14 18:14:33 +0900

    CleanCodeTools : function, property, include

---
+++ JavaScript/colors.js
@@ -0,0 +1,49 @@
+
+var Links = {
+	   setColor:function(color){
+			var alist = document.querySelectorAll('a');
+			var i = 0;
+				while(i < alist.length){
+				    alist[i].style.color=color;
+			    	i = i + 1 ;
+				}
+		   	}
+  		}
+   
+	var Body = {
+		setColor:function(color){
+			document.querySelector('body').style.color = color;
+		},		//객체의 property와 property를 구분할 때, ( , )콤마를 사용
+		setBackgroundColor:function(color){
+			document.querySelector('body').style.backgroundColor = color;
+		} 
+	}
+	
+	function nightDayHandler(self){
+			
+ 		var target = document.querySelector('body');
+ 	
+ 		if(self.value ==='night'){
+ 			Body.setBackgroundColor('black');
+ 			Body.setColor('white');
+  		
+ 			self.value = 'day';
+	
+  			Links.setColor('red');
+ 	
+ 		}else{
+ 			
+ 			Body.setBackgroundColor('white');
+ 			Body.setColor('black');
+  			
+ 			self.value = 'night';
+  	
+	  		var alist = document.querySelectorAll('a');
+			var i = 0;
+			
+			while(i < alist.length){
+			    alist[i].style.color='blue';
+			    i = i + 1;
+			}
+  		}
+ 	 }
