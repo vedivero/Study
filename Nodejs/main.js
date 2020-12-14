1949c217d25b52375d542968b15a176fa2f5921c
vedivero <zzangu0703@naver.com>
2020-12-15 00:43:43 +0900

    template

--- Nodejs/main.js
+++ Nodejs/main.js
@@ -6,19 +6,46 @@ var url = require('url');
     
 	var _url = request.url;
     var queryData = url.parse(_url, true).query;
-		
-    	console.log(queryData.id);
+	var title = queryData.id;
     
 	if(_url == '/'){
-      _url = '/index.html';
-    }
+		title = 'Welcome';
+	}
     
 	if(_url == '/favicon.ico'){
       return response.writeHead(404);
     }
     
 	response.writeHead(200);
-    response.end(queryData.id);
+	
+	var template = `
+		<!doctype html>
+		<html>
+			<head>
+				<title>WEB1 - ${title}</title>
+			<meta charset="utf-8">
+			</head>
+				<body>
+					<h1><a href="/">WEB</a></h1>
+		
+			<ol>
+			  <li><a href="/?id=HTML">HTML</a></li>
+			  <li><a href="/?id=CSS">CSS</a></li>
+			  <li><a href="/?id=JavaScript">JavaScript</a></li>
+			</ol>
+		
+		<h2>${title}</h2>
+		
+		HTML (HyperText Markup Language) is the most basic building block of the Web.
+		It defines the meaning and structure of web content.
+		Other technologies besides HTML are generally used to describe a web page's
+		appearance/presentation (CSS) or functionality/behavior (JavaScript).
+			
+			</body>
+		</html>
+	`;
+	
+    response.end(template);
  
 });
 app.listen(3000);
