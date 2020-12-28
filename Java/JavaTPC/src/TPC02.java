ed420efea391a5bbb9d2eac46227730a16ef9300
vedivero <zzangu0703@naver.com>
2020-12-28 14:34:30 +0900

    프래고래밍 3대 요소(변수,자료형,할당)

---
+++ Java/JavaTPC/src/TPC02.java
@@ -0,0 +1,17 @@
+
+public class TPC02 {
+
+	public static void main(String []args){
+	// 프로그래밍의 3대 요소 : 변수, 자료형(DataType), 할당(=)
+	// 1 + 1 = 2 /메모리에서 일정 공간이 할당되어 기억된다.
+	//변수 생성 = (1.크기 2.종류)결정
+	//타입이 정해져야 변수가 생성
+	
+	int a,b,c;
+
+	a=1;
+	b=2;
+	c=a+b;
+	System.out.println(c);
+	}
+}
