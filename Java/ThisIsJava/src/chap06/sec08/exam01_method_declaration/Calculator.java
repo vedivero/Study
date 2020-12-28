708821dae875bac0b8bf476c406c2d814fb9e057
vedivero <zzangu0703@naver.com>
2020-12-28 20:40:19 +0900

    6.8 method(1)

---
+++ Java/ThisIsJava/src/chap06/sec08/exam01_method_declaration/Calculator.java
@@ -0,0 +1,24 @@
+package chap06.sec08.exam01_method_declaration;
+
+public class Calculator {
+
+	//전원을 켜는 method - 매개값 없이 실행하는 method
+	void powerOn(){
+		System.out.println("Power On");
+	}
+	
+	int plus(int x,int y){
+		
+		int result = x + y;
+		
+		return result;
+	}
+	
+	double devide(int a,int b){
+		
+		
+		double result = (double)a / (double)b; 
+		
+		return result;
+	}
+}
