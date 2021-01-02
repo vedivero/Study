4f105db02f9a3d069044555ec7e2c5adcc8b166e
vedivero <zzangu0703@naver.com>
2021-01-02 11:57:19 +0900

    InteliJ tutorial

---
+++ InteliJ/inteliJTutorial/src/main/java/com/vedivero/inflearn/intelij/Main.java
@@ -0,0 +1,11 @@
+package com.vedivero.inflearn.intelij;
+
+public class Main {
+
+    //psvm
+    public static void main(String[] args) {
+        System.out.println("abc");
+
+        //run = ctrl + shift + F10
+    }
+}
