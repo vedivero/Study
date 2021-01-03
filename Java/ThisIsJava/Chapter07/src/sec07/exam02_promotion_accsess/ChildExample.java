7ea338fa0a0364b5413730714ea9adec824a04fa
vedivero <zzangu0703@naver.com>
2021-01-03 20:04:42 +0900

    review polymorphism

--- Java/ThisIsJava/Chapter07/src/sec07/exam02_promotion_accsess/ChildExample.java
+++ Java/ThisIsJava/Chapter07/src/sec07/exam02_promotion_accsess/ChildExample.java
@@ -12,11 +12,10 @@ public class ChildExample {
 		//Child에는 없는 method이므로 Parent의 method1이 호출
 		parent.method1();
 
-		//Child에서 재 정의된 method이므로 Type변환이 되더라도, '재 정의된 method가 호출'
+		//Type변환이 되더라도 Child에서 재 정의된 method가 호출
 		parent.method2();
 		
-		//Parent에는 없고 Child에만 있는 method이기에 사용 불가
-		//parent.method3();
+		//parent.method3();		//Parent에는 없고 Child에만 있는 method이기에 사용 불가
 		
 	}
 }
--- Java/ThisIsJava/Chapter07/src/sec07/exam04_array_management/Car.java
+++ Java/ThisIsJava/Chapter07/src/sec07/exam04_array_management/Car.java
@@ -3,7 +3,7 @@ package sec07.exam04_array_management;
 public class Car {
 	
 	//아래 코드를 Tire[]로 선언
-	//4개의 Field를 선언
+	//4개의 Field를  배열로 선언
 	
 	public Tire[] tires = {
 			
@@ -12,6 +12,7 @@ public class Car {
 			new Tire("left of Back Tire",3),
 			new Tire("right of Back",4)
 	};
+	
 	//각 Tire의 roll()를 호출하여 각 Tire를 회전 시킨다.
 	public int run(){
 		
@@ -22,8 +23,8 @@ public class Car {
 	
 		//대신해 for문을 사용
 		for(int i = 0; i<tires.length; i++){
-			//한 번 돌때마다 tire를 하나씩 호출
-			if(tires[i].roll()==false){	//tire가 펑크나면 자동차를 멈춘다.
+			//배열에서 index[i]를 하나씩 호출
+			if(tires[i].roll()==false){	//tire가 펑크나면 자동차를 멈춘다.	//roll() method = boolean Type
 				stop();
 					//문제되는 타이어의 순번을 return, 배열은 0부터 시작하니 +1
 					return (i+1); 
