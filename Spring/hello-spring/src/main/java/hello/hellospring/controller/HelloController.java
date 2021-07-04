6ea13a0446ecb86cad65b8de65cbb0c205c55c22
vedivero <zzangu0703@naver.com>
2021-07-04 18:34:53 +0900

    MVC-Template Engine & API

--- Spring/hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ Spring/hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,6 +3,8 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -13,4 +15,43 @@ public class HelloController {
         return "hello";
 
     }
+
+    @GetMapping("hello-mvc")
+    //외부에서 Parameter를 받는다
+    public String helloMvc(@RequestParam("name") String name,Model model){
+        model.addAttribute("name",name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name")String name){
+        return "hell " + name;    //"hello spring" <-요청한대로 client에 내려감
+    }
+    
+    //문자형이 아닌, Data를 요구하는 경우
+    //이 부분 때문에 API방식을 많이 사용한다.
+    //JSON방식 ( key : value )
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name")String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+
+    }
+
+    static class Hello{
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
+
 }
