a0a065dbec61e0b52ba0e1f8e1f9c7dd807f18cd
vedivero <zzangu0703@naver.com>
2021-07-14 18:59:12 +0900

    JPA

--- Spring/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ Spring/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,6 +1,7 @@
 package hello.hellospring;
 
 import hello.hellospring.repository.JdbcTemplateMemberRepository;
+import hello.hellospring.repository.JpaMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
@@ -8,16 +9,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.persistence.EntityManager;
 import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
-
-    private DataSource dataSource;
-
-    @Autowired
-    public SpringConfig(DataSource dataSource) {
+    private final DataSource dataSource;
+    private final EntityManager em;
+    public SpringConfig(DataSource dataSource, EntityManager em){
         this.dataSource = dataSource;
+        this.em = em;
     }
 
     @Bean
@@ -27,7 +28,8 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        //return new MemoryMemberRepository();
-        return new JdbcTemplateMemberRepository(dataSource);
-    }
+        //    return new MemoryMemberRepository();
+        //    return new JdbcMemberRepository(dataSource);
+        //    return new JdbcTemplateMemberRepository(dataSource);
+        return new JpaMemberRepository(em); }
 }
--- Spring/hello-spring/src/main/java/hello/hellospring/domain/Member.java
+++ Spring/hello-spring/src/main/java/hello/hellospring/domain/Member.java
@@ -1,7 +1,14 @@
 package hello.hellospring.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity //이제부터는 JPA가 관리하는 entity가 되는 것
 public class Member{
 
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;    //시스템이 부여하는 값
     private String name;    //고객이 회원가입 시 기재하는 이름
 
